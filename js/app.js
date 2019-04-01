b7cefc911f5dde90e2827084bd25b289dbbf1828
Yuan Gao <example@email.com>
2019-04-01 14:24:20 -0700

    update before 2nd branch

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+var hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
+
+
+var pike = {
+  startingData:{
+    minCus: 23
+    maxCus:65
+    ave: 6.3
+  }
+  customers: [];
+}
