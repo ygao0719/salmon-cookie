1278eedc156b8b03e9f6d3ab2ef61f953d68510c
Yuan Gao <example@email.com>
2019-04-03 08:42:24 -0700

    update for tuesday lab

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 'use strict';
 var hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
-
+var storeCookiePerHour = [];
 var allStore = [];
+
 function Store (locationName,minCus,maxCus,ave) {
   this.locationName = locationName;
   this.minCus = minCus;
@@ -11,11 +12,11 @@ function Store (locationName,minCus,maxCus,ave) {
   this.people=[];
   allStore.push(this);
 }
-new Store('1st and Pike','23','65','6.3');
-new Store('SeaTac Airport','3','24','1.2');
-new Store('Seattle Center','11','38','3.7');
-new Store('Capitol Hill','20','38','2.3');
-new Store('Alki','2','16','4.6');
+new Store('1st and Pike',23,65,6.3);
+new Store('SeaTac Airport',3,24,1.2);
+new Store('Seattle Center',11,38,3.7);
+new Store('Capitol Hill',20,38,2.3);
+new Store('Alki',2,16,4.6);
 
 
 Store.prototype.customers=function(){
@@ -65,7 +66,7 @@ Store.prototype.render=function(){
 };
 
 //create a table frame.
-function tableForStores(){
+function tableHead(){
   var storeTable = document.getElementById('main');
   var trEl = document.createElement('tr');
   var thEl = document.createElement('th');
@@ -85,10 +86,10 @@ function tableForStores(){
     }
   }
 
-  var footer = storeTable.createTFoot();
-  var row = footer.insertRow(0);
-  var cell = row.insertCell(0);
-  cell.innerHTML = '<b>Total</b>';
+  // var footer = storeTable.createTFoot();
+  // var row = footer.insertRow(0);
+  // var cell = row.insertCell(0);
+  // cell.innerHTML = '<b>Total</b>';
 }
 
 function renderStore() {
@@ -107,14 +108,24 @@ function totalAtFooter(){
     for (var j =0; j < allStore.length; j++){
       totalCookiePerHour += allStore[j].cookiesPerHour[i];
     }
-    var trEl = document.createElement('tr');
-    var tdEl = document.createElement('td');
-    tdEl.textContent = totalCookiePerHour[i];
+    storeCookiePerHour.push(totalCookiePerHour);
+  }
+  var footData = document.getElementById('main');
+  var trEl = document.createElement('tr');
+  var tdEl = document.createElement('td');
+  tdEl.textContent = 'Total';
+  trEl.appendChild(tdEl);
+
+  for (var i = 0; i< hours.length; i++){
+    tdEl = document.createElement('td');
+    tdEl.textContent = storeCookiePerHour[i];
     trEl.appendChild(tdEl);
   }
+  footData.appendChild(trEl);
 }
 
 
 
-tableForStores();
+tableHead();
 renderStore();
+totalAtFooter();
