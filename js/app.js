d12a35fb804cb317d3b1f2bb71926602f4811584
Yuan Gao <example@email.com>
2019-04-07 16:15:56 -0700

    sunday update

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 'use strict';
+
 var hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
 // var storeCookiePerHour = [];
 var allStore = [];
@@ -146,6 +147,7 @@ function handleNewStore(event){
   totalAtFooter();
   boomRow();
 }
+//this deletes duplicate total
 function boomRow(){
   var totalRow = document.getElementById('total-row');
   if (totalRow ){
